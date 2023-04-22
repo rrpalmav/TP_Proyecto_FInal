036f1c4ae6344267958029aef30adece6fff09a0
rrpalmav <rrpalmav@gmail.com>
2023-04-22 01:26:09 -0600

    Update lib_dht11.h

--- Drivers/API/inc/lib_dht11.h
+++ Drivers/API/inc/lib_dht11.h
@@ -19,17 +19,15 @@ typedef enum {
 	ERROR_DHT11
 } dht11State_t;
 
-
 void dht11_init (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 void dht11_start();
 uint8_t dht11_check_response (void);
 uint8_t dht11_read_data (void);
-
 void set_pin_output (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 void set_pin_input (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 
-
 void set_GPIOx_Port(GPIO_TypeDef *GPIOx);
 void set_GPIOx_Port_Pin(uint16_t GPIO_Pin);
+void dht11_state();
 
 #endif /* INC_LIB_DHT11_H */

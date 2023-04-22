9b7f207aea6569197bbc5742367a30c62e04f066
rrpalmav <rrpalmav@gmail.com>
2023-04-22 01:26:56 -0600

    Update lib_lcd_16x02_i2c.h

--- Drivers/API/inc/lib_lcd_16x02_i2c.h
+++ Drivers/API/inc/lib_lcd_16x02_i2c.h
@@ -21,15 +21,12 @@ typedef enum {
 	ERROR_LCD_16x02
 } lcd_16x02State_t;
 
-
-
 void lcd_16x02_init (void);   // inicializa lcd_16x02
 void lcd_16x02_send_cmd (char cmd);  // envia commando a la lcd_16x20
 void lcd_16x02_send_data (char data);  // envia data a la lcd_16x02
 void lcd_16x02_send_string (char *str);  // envia cadenas a la ldc_16x02
 void lcd_16x02_gotoxy(int y, int x);  // posiciona el curso ne la posicion fila (1 or 2), col (1-16);
 void lcd_16x02_clear (void);
-
-
+void lcd_16x02_state(int msg);
 
 #endif /* LIB_LCD_16X02_I2C_H_ */
